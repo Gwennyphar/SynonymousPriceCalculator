7dafe35521468a59a128e149a1128662e3f86eff
Martin Sonnhauser <synonymous1984@gmail.com>
2016-09-28 21:58:58 +0200

    Added own price calculation when articles is added to basket

--- SynonymousPriceCalculator.php
+++ SynonymousPriceCalculator.php
@@ -3,6 +3,8 @@
 namespace SynonymousPriceCalculator;
 
 use Shopware\Components\Plugin;
+use Shopware\Models\Article\Article;
+use Shopware\Models\Order\Basket;
 
 /**
  * Class SynonymousPriceCalculator
@@ -18,7 +20,8 @@ class SynonymousPriceCalculator extends Plugin
     {
         return [
             'Enlight_Controller_Action_PostDispatchSecure' => 'addTemplateDir',
-            'Shopware_Plugins_HttpCache_GetCacheIds' => 'filterHttpCacheId'
+            'Shopware_Plugins_HttpCache_GetCacheIds' => 'filterHttpCacheId',
+            'Shopware_Modules_Basket_UpdateArticle_FilterSqlDefault' => 'updateBasketArticleFilterSql'
         ];
     }
 
@@ -58,5 +61,43 @@ class SynonymousPriceCalculator extends Plugin
 
     }
 
+    /**
+     * Due to the fact that the cart is on the old system and uses an own price calculation
+     * algorithm and not the price_calculation_service, we have to emit the event
+     * Shopware_Modules_Basket_UpdateArticle_FilterSqlDefault and manipulate the SQL query
+     * which is executed each time a product is added to or updated in cart.
+     *
+     * @param \Enlight_Event_EventArgs $args
+     * @return string
+     */
+    public function updateBasketArticleFilterSql(\Enlight_Event_EventArgs $args) {
+
+        // Get the shop context
+        $context = $this->container->get('shopware_storefront.context_service')->getContext();
+
+        // Get orderNumber of article for order_basket id
+        $orderBasketArticle = $this->container->get('models')->getRepository(Basket::class)->find($args->get('id'));
+        $orderNumber = $orderBasketArticle->getOrderNumber();
+
+        // Use the productService to get the product with prices calculated by the
+        // price_calculation_service which is decorated by this plugin.
+        $productService = Shopware()->Container()->get('shopware_storefront.product_service');
+        $product = $productService->get($orderNumber,$context);
+
+        $cheapestPrice = $product->getCheapestPrice();
+
+        // Add some dummy prices here
+        $price = $cheapestPrice->getCalculatedPrice();
+        $netPrice = $cheapestPrice->getRule()->getPrice();
+
+        // Manipulate the query so that the correct price is set on the statement
+        $query = "
+            UPDATE s_order_basket
+            SET quantity = ?, price = ?, netprice = ?, price = ".$price.", netprice = ".$netPrice.", currencyFactor = ?, tax_rate = ?
+            WHERE id = ? AND sessionID = ? AND modus = 0";
+
+        return $query;
+
+    }
 
 }
